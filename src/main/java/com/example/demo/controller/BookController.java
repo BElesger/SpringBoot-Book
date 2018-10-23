cb88d962c457437127a7c4eae407131c2aa74d47
Alasgar Baghirli <bagirlielesger@gmail.com>
2018-10-23 14:46:48 +0400

    add post (save) Method

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,5 +26,12 @@ public class BookController {
         book.add(new Book("Dan Brown","Origin"));
         book.add(new Book("Dan Brown","Origin"));
         return book;
+    }
+
+    @RequestMapping(method= RequestMethod.POST,value ="/save")
+    public String saveBook(@RequestBody Book b){
+
+        return "Your "+b.getBookName()+"book   saved"  ;
+
     }
 }
